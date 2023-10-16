ba4c8120e39fb091050921263f8fd4a1879b1d1f
travizanutto <ctravizanutto@gmail.com>
2023-10-16 14:40:13 -0300

    new ast functions

--- src/ast.h
+++ src/ast.h
@@ -3,6 +3,7 @@
 #include <ctype.h>
 #include <string.h>
 
+// bison and flex related declarations and definitions
 extern int yylineno;
 extern FILE* yyin;
 int yyerror(const char* s, ...);
@@ -11,11 +12,44 @@ int yylex();
     yydebug = 1;
 #endif
 
+typedef enum {
+    _SIMPLE,
+    _AND,
+    _OR,
+    _NOT,
+    _AND_NOT,
+    _OR_NOT,
+} context_type;
+
+typedef struct leaf {
+    char leaf_name[256];
+    struct leaf *next;
+} leaf;
+
+typedef struct context {
+    char context_name[256];
+    context_type type;
+    struct context *next;
+} context;
+
+typedef struct plan {
+    char plan_name[256];
+    char trigger_name[256];
+    context *contexts;
+    struct leaf *actions;
+    struct plan *next;
+} plan;
+
+typedef struct agent {
+    char agent_name[256];
+    struct leaf *beliefs;
+    struct leaf *goals;
+    struct plan *plans;
+    struct agent *next;
+} agent;
 
 /* function by: Jonathan Leffler
-https://stackoverflow.com/questions/16136437/clean-all-spaces-and-newlines-c-or-lex-bison
-*/
-
+https://stackoverflow.com/questions/16136437/clean-all-spaces-and-newlines-c-or-lex-bison */
 static void squish_whitespace(char *TAG)
 {
     int sizeOfArray = strlen(TAG);
@@ -33,3 +67,57 @@ static void squish_whitespace(char *TAG)
     }
     TAG[j] = '\0';
 }
+
+leaf* new_leaf(char name[256], leaf* next) {
+    leaf *tmp = (leaf*) malloc(sizeof(leaf));
+    if (!tmp) {
+        printf("Couldn't allocate leaf %s at %i.\n", name, yylineno);
+        exit(EXIT_FAILURE);
+    }
+    strlcpy(tmp->leaf_name, name, 255);
+    tmp->next = next;
+    return tmp;
+}
+
+
+context* new_context(char name[256], context_type type, context *next) {
+    context *tmp = (context*) malloc(sizeof(context));
+    if (!tmp) {
+        printf("Couldn't allocate context %s at %i.\n", name, yylineno);
+        exit(EXIT_FAILURE);
+    }
+    strlcpy(tmp->context_name, name, 255);
+    tmp->type = type;
+    tmp->next = next;
+    return tmp;
+}
+
+plan* new_plan(char plan_name[256], char trigger_name[256], context *contexts, leaf *actions, plan *next) {
+    plan *tmp = (plan*) malloc(sizeof(plan));
+        if (!tmp) {
+        printf("Couldn't allocate plan %s at %i.\n", plan_name, yylineno);
+        exit(EXIT_FAILURE);
+    }
+    strlcpy(tmp->plan_name, plan_name, 255);
+    strlcpy(tmp->trigger_name, trigger_name, 255);
+    tmp->contexts = contexts;
+    tmp->actions = actions;
+    tmp->next = next;
+    return tmp;
+}
+
+agent* new_agent(char name[256], leaf *beliefs, leaf *goals, plan *plans, agent *next) {
+    agent *tmp = (agent*) malloc(sizeof(agent));
+    if (!tmp) {
+        printf("Couldn't allocate agent %s at %i.\n", name, yylineno);
+        exit(EXIT_FAILURE);
+    }
+    strlcpy(tmp->agent_name, name, 255);
+    tmp->beliefs = beliefs;
+    tmp->goals = goals;
+    tmp->plans = plans;
+    tmp->next = next;
+    return tmp;
+}
+
+void eval(agent *agent);
--- src/parse.y
+++ src/parse.y
@@ -4,53 +4,60 @@
 
 %union {
     char s[256];
+    struct leaf *l;
+    struct context *c;
+    struct plan *p;
+    struct agent *a;
 }
 
-%token <s> NAME AGENT_NAME
+%token<s> NAME AGENT_NAME
 %token BELIEFS GOALS PLANS AND OR NOT
-%token PERCENT OPEN_CURLY_BRACKET CLOSE_CURLY_BRACKET OPEN_PARENTHESIS CLOSE_PARENTHESIS SEMICOLON NEW_LINE
+%token PERCENT OPEN_CURLY_BRACKET CLOSE_CURLY_BRACKET OPEN_PARENTHESIS CLOSE_PARENTHESIS SEMICOLON
+
+%type<s> agent_name list_element_name plan_name plan_trigger
+%type<a> agent_list
+%type<l> simple_list beliefs goals plan_actions
+%type<p> plans plan_set
+
 
 %start program
 
 %%
-program: 
+program: {}
     | agent_list program
     ;
 
-agent_list: 
-    | agent PERCENT agent_list 
-    ;
-// agent_name beliefs goals
-agent: agent_name beliefs goals plans { printf("agent\n"); }
+agent_list: { $$ = NULL; }
+    | agent_name beliefs goals plans PERCENT agent_list { $$ = new_agent($1, $2, $3, $4, $6); }
     ;
 
-agent_name: AGENT_NAME
+agent_name: AGENT_NAME { strlcpy($$, $1, 255); }
     ;
 
-beliefs: BELIEFS OPEN_CURLY_BRACKET simple_list CLOSE_CURLY_BRACKET
+beliefs: BELIEFS OPEN_CURLY_BRACKET simple_list CLOSE_CURLY_BRACKET { $$ = $3; }
     ;
 
-goals: GOALS OPEN_CURLY_BRACKET simple_list CLOSE_CURLY_BRACKET
+goals: GOALS OPEN_CURLY_BRACKET simple_list CLOSE_CURLY_BRACKET { $$ = $3; }
     ;
 
-simple_list:
-    | list_element_name SEMICOLON simple_list
+simple_list: { $$ = NULL; }
+    | list_element_name SEMICOLON simple_list { new_leaf($1, $3); }
     ;
 
-list_element_name: NAME { }
+list_element_name: NAME { strlcpy($$, $1, 255); }
     ;
 
-plans: PLANS OPEN_CURLY_BRACKET plan_set CLOSE_CURLY_BRACKET
+plans: PLANS OPEN_CURLY_BRACKET plan_set CLOSE_CURLY_BRACKET { $$ = $3; }
     ;
 
-plan_set: 
-    | plan_name OPEN_PARENTHESIS plan_trigger context plan_actions CLOSE_PARENTHESIS SEMICOLON plan_set
+plan_set: { $$ = NULL; }
+    | plan_name OPEN_PARENTHESIS plan_trigger context plan_actions CLOSE_PARENTHESIS SEMICOLON plan_set { }
     ;
 
-plan_name: NAME { }
+plan_name: NAME { strlcpy($$, $1, 255);1; }
     ;
 
-plan_trigger: NAME SEMICOLON { }
+plan_trigger: NAME SEMICOLON { strlcpy($$, $1, 255); }
     ;
 
 context: context_and AND context_and SEMICOLON
@@ -81,7 +88,11 @@ plan_actions: OPEN_CURLY_BRACKET simple_list CLOSE_CURLY_BRACKET
 
 // ---------------------------------
 
-int main() {
+int main(int argc, int **argv) {
+    if (argc < 1) {
+        printf("Usage: ./nag_to_jacamo \"list of files\"");
+        exit(EXIT_FAILURE);
+    }
     yyparse();
 }
 
